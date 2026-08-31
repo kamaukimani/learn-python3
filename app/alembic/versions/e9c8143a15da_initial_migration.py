4c20e66280824b9f5a55285d81fe6d20e751bd8c
kamaukimani <paulkimys285@gmail.com>
2026-08-31 06:30:46 +0300

    initial migration

---
+++ app/alembic/versions/e9c8143a15da_initial_migration.py
@@ -0,0 +1,26 @@
+"""initial migration
+
+Revision ID: e9c8143a15da
+Revises: 
+Create Date: 2026-08-31 06:27:02.419926
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'e9c8143a15da'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
